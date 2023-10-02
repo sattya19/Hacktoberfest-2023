9e81b45fcc87b71fbfe67f133a7ca741909d73ec
Rohan7996 <116304597+Rohan7996@users.noreply.github.com>
2023-10-02 19:01:01 +0530

    Create insertsort.java

---
+++ insertsort.java
@@ -0,0 +1,22 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+		int[] a={20,18,16,17,14};
+		int ele,j;
+		for(int i=1;i<5;i++){
+		    ele=a[i];
+		    j=i-1;
+		    while(j>=0&&a[j]>ele){
+		        a[j+1]=a[j];
+		        j--;
+		    }
+		    a[j+1]=ele;
+		}
+		System.out.print("sorting :");
+		for(int i=0;i<5;i++){
+		    System.out.print(a[i]+" ");
+		}
+	}
+}
+
